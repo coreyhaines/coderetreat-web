398592ea0985b38d67fb96df2167879ac1c35389
gustin <gustin@entryway.net>
2012-04-25 10:16:07 -0700

    Floyd 2012 update   - Site is geared toward Floyd for now.   - Information update for 2012   - New background that hopefully loads efficiently.   - Current sponsor update.

--- public/javascripts/floyd.js
+++ public/javascripts/floyd.js
@@ -3,16 +3,19 @@ $(function() {
 
 //$('a#save-our-driveway').lightBox();
 
-//$('a#blue-ridge-pic').lightBox(); 
+//$('a#blue-ridge-pic').lightBox();
 
-  $('a.fancybox').fancybox({ 
-    'hideOnContentClick': true, 
-    'overlayShow': false,
-    'zoomSpeedIn': 600,
-    'zoomSpeedOut': 500,
-    'easingIn': 'easeOutBack',
-    'easingOut': 'easeInBack',
-    'overlayColor': '#fff'
+  $('a.fancybox').fancybox({
+//    'hideOnContentClick': true,
+//    'overlayShow': true,
+//    'zoomSpeedIn': 600,
+//    'zoomSpeedOut': 500,
+//    'easingIn': 'easeOutBack',
+//   'easingOut': 'easeInBack',
+//    'overlayColor': '#fff',
+//    'autoScale': true,
+//    'autoDimensions': true,
+//    'modal': true
   });
 
   $('.iframe').fancybox({
